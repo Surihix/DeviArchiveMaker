fbcee23f776accec5c38e10b313ad847aa837ed0
Surihix <neweramysterymailer@gmail.com>
2024-05-31 19:22:36 +0530

    help message string updated

--- DeviArchiveMaker/Help.cs
+++ DeviArchiveMaker/Help.cs
@@ -15,13 +15,14 @@ namespace DeviArchiveMaker
             Console.WriteLine("-? or -h = Show app functions");
             Console.WriteLine("");
             Console.WriteLine("");
-            Console.WriteLine("When using -p function, you will have to specify a compression level argument switch");
+            Console.WriteLine("When using -p function, you will have to specify a compression level argument switch.");
+            Console.WriteLine("-c1 to -c3 will apply zlib compression.");
             Console.WriteLine("");
             Console.WriteLine("Valid compression levels:");
             Console.WriteLine("-c0 = No compression");
             Console.WriteLine("-c1 = Fastest compression");
             Console.WriteLine("-c2 = Optimal compression");
-            Console.WriteLine("-c3 = Smallest size");
+            Console.WriteLine("-c3 = Best compression");
             Console.WriteLine("");
             Console.WriteLine("Usage Examples:");
             Console.WriteLine("To Pack a folder: DeviArchiveMaker -p " + @"""Folder To pack""" + " -c3");
